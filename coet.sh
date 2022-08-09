47292d4173a58a44e96f4b420a9bc0bea9fc05ea
guabangetdahlu55 <110933948+guabangetdahlu55@users.noreply.github.com>
2022-08-10 02:28:44 +0700

    Update coet.sh

--- coet.sh
+++ coet.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-lscpu
+set -e
+
+# Customize this script to provide all the necessary steps to prepare your
+# source files.
+autoconf
+./configure
+
+# In case you need to compile everything,
+bear make
+
+# Or add external required libraries sources
+git clone --depth 1 --branch v1.1.3 \
+  https://github.com/madler/zlib.git ./lib/zlib
